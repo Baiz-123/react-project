f3340661fdd992beb36eb799daca8baeb0ef267e
Baiz-123 <67458565+Baiz-123@users.noreply.github.com>
2020-07-08 19:08:01 +0530

    Add files via upload

---
+++ TransactionList.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import TransactionForm from './TransactionForm'
+import { connect } from "react-redux";
+import * as actions from "../actions/transactionActions";
+import { bindActionCreators } from "redux";
+
+class TransactionList extends Component {
+
+
+    handleEdit = (index) => {
+        this.props.updateTransactionIndex(index)
+    }
+
+    handleDelete = (index) => {
+        this.props.deleteTransaction(index)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <TransactionForm />
+                <hr />
+                <table className="tableDesign">
+                    <tbody>
+                        <tr><th>Bank Account No</th>
+                        <th>IFSC Code</th>
+                        <th>Account Holder Name</th>
+                        <th>Amount</th></tr>
+                        {this.props.list.map((item, index) => {
+                            return <tr key={index}>
+                                <td>{item.bAccountNo}</td>
+                                <td>{item.iFSC}</td>
+                                <td>{item.bName}</td>
+                                <td>{item.amount}</td>
+                                <td><button onClick={() => this.handleEdit(index)} className="myListButton">Edit</button></td>
+                                <td><button onClick={() => this.handleDelete(index)} className="myListButton">Delete</button></td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        list: state.list
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        updateTransactionIndex: actions.updateIndex,
+        deleteTransaction: actions.Delete
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TransactionList)
